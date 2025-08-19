94341ef1ea659e79db5323a1f0e5392de1a10d39
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-19 12:00:45 -0700

    StringFormatting

--- Java/Day_1/StringFormatting.java
+++ Java/Day_1/StringFormatting.java
@@ -5,7 +5,9 @@ public class StringFormatting {
         String name = "Linus";
         int age = 24;
         String language = "Java";
-        String formattedString = String.format("Hello %s! You are %d years old and you love %s.", name, age, language);
-        System.out.println(formattedString);
+        System.out.println(String.format("Hello %s! You are %d years old and you love %s.", name, age, language));
+
+         double pi = 3.14159;
+        System.out.println(String.format("Pi to 2 decimal places: %.2f", pi));
     }
 }
