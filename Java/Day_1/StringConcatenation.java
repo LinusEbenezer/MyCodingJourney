46cc344931bba2ffefa58e00bf6856012fd90482
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-19 11:49:06 -0700

    String Concatenation

---
+++ Java/Day_1/StringConcatenation.java
@@ -0,0 +1,12 @@
+package MyCodingJourney.Java.Day_1;
+
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String firstName = "Linus";
+        String lastName = "Ebenezer";
+        String city = "Loma Linda";
+        String country = "USA";
+        int age = 24;
+        System.out.println("First Name: " + firstName +"lastName: "+ lastName + "city: "+ city + "country: "+ country + "age: "+ age);
+}
+}
