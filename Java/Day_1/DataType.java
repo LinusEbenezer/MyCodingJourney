2a70b6abaa74d2825e8e8c945797253cef6061b2
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-19 03:28:17 -0700

    DataType

---
+++ Java/Day_1/DataType.java
@@ -0,0 +1,24 @@
+package MyCodingJourney.Java.Day_1;
+
+public class DataType {
+public static void main(String[] args) {
+        int age = 25; 
+        byte byteValue = 100;
+        short shortValue = 30000;
+        long population = 7800000000L;
+        float height = 5.9f;
+        double salary = 50000.50;
+        char initial = 'A';
+        boolean isEmployed = true;
+        String name = "John Doe";
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Byte Value: " + byteValue);
+        System.out.println("Short Value: " + shortValue);
+        System.out.println("Long Population: " + population);
+        System.out.println("Height: " + height);
+        System.out.println("Salary: " + salary);
+        System.out.println("Initial: " + initial);
+        System.out.println("Is Employed: " + isEmployed);
+     }
+}
