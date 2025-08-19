9368f9ddb0d46f90109c9f48d6548873900969a5
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-19 04:16:04 -0700

    day1

---
+++ Java/Day_1/Operators.java
@@ -0,0 +1,17 @@
+package MyCodingJourney.Java.Day_1;
+public class Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b)); // integer division
+        System.out.println("Remainder: " + (a % b));
+
+        double x = 10;
+        double y = 3;
+        System.out.println("Division (double): " + (x / y));
+    }
+}
---
+++ Java/Day_1/variable.java
@@ -0,0 +1,25 @@
+package MyCodingJourney.Java.Day_1;
+
+public class variable {
+    public static void main(String[] args) {
+        int age = 30; 
+        byte byteValue = 120;
+        short shortValue = 25000;
+        long population = 8000000000L;
+        float height = 6.1f;
+        double salary = 60000.75;
+        char initial = 'B';
+        boolean isEmployed = false;
+        String name = "Jane Smith";
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Byte Value: " + byteValue);
+        System.out.println("Short Value: " + shortValue);
+        System.out.println("Long Population: " + population);
+        System.out.println("Height: " + height);
+        System.out.println("Salary: " + salary);
+        System.out.println("Initial: " + initial);
+        System.out.println("Is Employed: " + isEmployed);
+    }
+}
