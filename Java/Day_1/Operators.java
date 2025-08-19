5bc37dd6344d2e0eade0a3d1e5172671a0603275
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-19 11:10:20 -0700

    Operators

--- Java/Day_1/Operators.java
+++ Java/Day_1/Operators.java
@@ -1,15 +1,14 @@
 package MyCodingJourney.Java.Day_1;
+//arithmetic operators in Java
 public class Operators {
     public static void main(String[] args) {
         int a = 10;
-        int b = 3;
-
+        int b = 5;
         System.out.println("Addition: " + (a + b));
         System.out.println("Subtraction: " + (a - b));
         System.out.println("Multiplication: " + (a * b));
         System.out.println("Division: " + (a / b)); // integer division
         System.out.println("Remainder: " + (a % b));
-
         double x = 10;
         double y = 3;
         System.out.println("Division (double): " + (x / y));
