b5f2f89b6267507e005249c8d891b86e14ca81cd
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-19 10:53:10 -0700

    task 2

---
+++ Java/Day_1/Task2.java
@@ -0,0 +1,29 @@
+package MyCodingJourney.Java.Day_1;
+/*Write a Java program that:
+Takes two integers from the user
+Prints their sum, difference, product, quotient, and remainder */
+import java.util.Scanner;
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter first integer: ");
+        int a = in.nextInt();
+        System.out.print("Enter second integer: ");
+        int b = in.nextInt();
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Product: " + (a * b));
+        if (b == 0) {
+            System.out.println("Division by zero is not allowed.");
+        } else {
+            System.out.println("Division: " + (a / b));
+            
+        }
+        if (b == 0) {
+            System.out.println("Remainder by zero is not allowed.");
+        } else {
+        System.out.println("Remainder: " + (a % b));
+        }
+        in.close();
+    }
+}
