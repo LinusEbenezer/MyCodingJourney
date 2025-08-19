e53d0911009b58207664e6b21a7b6444a7e6b4dc
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-19 01:53:43 -0700

    Data types

---
+++ Java/Day1/DataType.java
@@ -0,0 +1,24 @@
+package Java.Day1;
+
+public class DataType {
+    public static void main(String[] args) {
+    int number = 10;
+    short smallNumber = 5;
+    long largeNumber = 100000L;
+    float decimalNumber1 = 15.5f;
+    double decimalNumber2 = 20.5;
+    char letter = 'A';
+    boolean isTrue = true;
+    String text = "Hello, World!";
+
+    System.out.println("Integer: " + number);
+    System.out.println("Short: " + smallNumber);
+    System.out.println("Long: " + largeNumber);
+    System.out.println("Float: " + decimalNumber1);
+    System.out.println("Double: " + decimalNumber2);
+    System.out.println("Character: " + letter);
+    System.out.println("Boolean: " + isTrue);
+    System.out.println("String: " + text);
+    }
+
+}
