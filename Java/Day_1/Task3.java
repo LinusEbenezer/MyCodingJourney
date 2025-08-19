97343d2aa2a3e73a9dc8928a274cee85c750f64e
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-19 11:12:05 -0700

    Swaping without 3rd variable

---
+++ Java/Day_1/Task3.java
@@ -0,0 +1,20 @@
+package MyCodingJourney.Java.Day_1;
+/* Write a Java program that:
+Declares two variables a and b with any values
+Swaps them without using a third variable
+Prints values before and after the swap */
+public class Task3 {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before Swap: a = " + a + ", b = " + b);
+
+        // Swapping without using a third variable
+        a = a + b; 
+        b= a - b;
+        a = a - b;
+        System.out.println("After Swap: a = " + a + ", b = " + b);
+    }
+}
