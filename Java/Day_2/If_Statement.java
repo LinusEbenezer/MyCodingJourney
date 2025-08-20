f9258f28ef0c700a07ec8a8f6739fa495a3bc1c6
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-20 01:48:54 -0700

    Control Statements-if

---
+++ Java/Day_2/If_Statement.java
@@ -0,0 +1,10 @@
+package MyCodingJourney.Java.Day_2;
+
+public class If_Statement {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        }
+    }
+}
