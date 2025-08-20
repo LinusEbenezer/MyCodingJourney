caf5d0c6c79e4d8587c36f675ca30d471c2b2ebe
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-20 01:47:48 -0700

    Comparision Operators

---
+++ Java/Day_2/ComparisionOperators.java
@@ -0,0 +1,15 @@
+package MyCodingJourney.Java.Day_2;
+
+public class ComparisionOperators {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+
+        System.out.println(a == b); // false
+        System.out.println(a != b); // true
+        System.out.println(a > b);  // true
+        System.out.println(a < b);  // false
+        System.out.println(a >= b); // true
+        System.out.println(a <= b); // false
+    }
+}
+
