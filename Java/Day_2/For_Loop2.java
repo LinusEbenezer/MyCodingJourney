a220e58b4ffdbd2b06211c08c091b83d9aae8e05
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-20 01:50:51 -0700

    For loop - Printing Even Numbers

--- Java/Day_2/For_Loop2.java
+++ Java/Day_2/For_Loop2.java
@@ -1,5 +1,9 @@
 package MyCodingJourney.Java.Day_2;
 
-public class ForLoop2 {
-    public static void main(String[] args) 
+public class For_Loop2 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+    }
 }
