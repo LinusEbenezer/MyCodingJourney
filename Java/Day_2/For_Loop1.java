c53539172f8b2a82eb489ce6697a3969412538d3
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-20 01:51:48 -0700

    For loop Counting 1 to 5

---
+++ Java/Day_2/For_Loop1.java
@@ -0,0 +1,9 @@
+package MyCodingJourney.Java.Day_2;
+
+public class For_Loop1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+        System.out.println("Count: " + i);
+        }
+    }
+}
