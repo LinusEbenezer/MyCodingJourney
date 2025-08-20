bfb355b7da806b0e37d025f9e147eec7d21d35ee
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-20 01:50:03 -0700

    Control Statements-else_if

---
+++ Java/Day_2/Else_If_Ladder.java
@@ -0,0 +1,18 @@
+package MyCodingJourney.Java.Day_2;
+
+public class Else_If_Ladder {
+    public static void main(String[] args) {
+        int score = 85;
+
+if (score >= 90) {
+    System.out.println("Grade A");
+} else if (score >= 75) {
+    System.out.println("Grade B");
+} else if (score >= 50) {
+    System.out.println("Grade C");
+} else {
+    System.out.println("Fail");
+}
+
+    }
+}
