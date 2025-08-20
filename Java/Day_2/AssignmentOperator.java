06ca1762cd257c92c7c85fbf68c2d09cee6e302b
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-20 01:47:07 -0700

    Assignment Operators

---
+++ Java/Day_2/AssignmentOperator.java
@@ -0,0 +1,17 @@
+package MyCodingJourney.Java.Day_2;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int num = 10;
+
+        num += 5; // num = 15
+        num -= 3; // num = 12
+        num *= 2; // num = 24
+        num /= 4; // num = 6
+        num %= 4; // num = 2
+
+        System.out.println("Final num: " + num);
+    }
+}
+
+
