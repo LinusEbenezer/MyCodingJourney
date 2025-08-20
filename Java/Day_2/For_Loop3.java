28b5b8ad043fe98cc269d275372a1b2e76bdf0ae
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-20 01:46:34 -0700

    For loop - Looping Backwards

---
+++ Java/Day_2/For_Loop3.java
@@ -0,0 +1,10 @@
+package MyCodingJourney.Java.Day_2;
+
+public class For_Loop3 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+        System.out.println(i);
+       }
+
+    }
+}
