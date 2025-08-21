261453938096363798864f470b310791c46a4968
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-20 22:00:31 -0700

    While loop - count untill the condition is true

---
+++ Java/Day_2/While_Loop1.java
@@ -0,0 +1,11 @@
+package MyCodingJourney.Java.Day_2;
+
+public class While_Loop1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+        System.out.println("Count: " + i);
+        i++;
+       }
+    }
+}
