8c6168e53522a9284e138fa27b8727d859dfb6b7
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-20 21:58:10 -0700

    lb to kg conversion

---
+++ Java/Day_1/Task6.java
@@ -0,0 +1,11 @@
+package MyCodingJourney.Java.Day_1;
+// convert a given number of pounds to kilograms
+public class Task6 {
+    public static void main(String[] args) {
+        double pounds = 10; 
+        double kilograms = pounds * 0.45359237; 
+        System.out.println(pounds + " pounds is equal to " + kilograms + " kilograms.");
+
+    }
+    }
+
