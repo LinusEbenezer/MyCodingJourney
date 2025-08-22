7b7619514d769e40d385b0f40f370051cb693d0c
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-21 23:24:35 -0700

    Nested For Loop

---
+++ Java/Day_2/Nested_Loop.java
@@ -0,0 +1,12 @@
+package MyCodingJourney.Java.Day_2;
+
+public class Nested_Loop {
+    public static void main(String[] args) {
+    for (int i = 1; i <= 3; i++) {
+    for (int j = 1; j <= 3; j++) {
+        System.out.println(i + " * " + j + " = " + (i * j));
+    }
+    System.out.println(); // Blank line between tables
+  }
+ }
+}
