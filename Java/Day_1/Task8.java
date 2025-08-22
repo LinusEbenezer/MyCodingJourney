43fb415ebee7412621a3c93808505fd0d51c5a0b
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-21 23:07:16 -0700

    Compund Assignment operator Challenge

---
+++ Java/Day_1/Task8.java
@@ -0,0 +1,9 @@
+package MyCodingJourney.Java.Day_1;
+
+public class Task8 {
+    public static void main(String[] args) {
+        int result =10;
+        result -=5.5;
+        System.out.println("Result after subtraction: " + result); //The result will be 4
+    }
+}
