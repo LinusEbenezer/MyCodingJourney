a913dbdd333f1a885d3d0ebeb270ab2f35343d1e
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-25 22:34:08 -0700

    Find Sum,Average,Largest & Smallest

--- Java/Day_3/Task_1.java
+++ Java/Day_3/Task_1.java
@@ -1,13 +1,13 @@
 package MyCodingJourney.Java.Day_3;
    import java.util.Scanner;
-public class Task_2 {
- 
-
-
+public class Task_1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         
-        int[] numbers = new int[5]; // array of size 5
+       System.out.print("How many numbers do you want to enter? ");
+        int n = in.nextInt();
+
+        int[] numbers = new int[n]; // array size depends on user input
 
         // Input
         for (int i = 0; i < numbers.length; i++) {
