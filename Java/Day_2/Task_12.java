3428e9d6960f312323bb781b7206bf53481fb7fb
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-27 14:14:21 -0700

    Find The Alphebet case

---
+++ Java/Day_2/Task_12.java
@@ -0,0 +1,17 @@
+package MyCodingJourney.Java.Day_2;
+   import java.util.Scanner;
+public class Task_12 {
+ 
+
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      char ch = in.next().trim().charAt(0);
+       if ((ch >= 'a')&& (ch<='z')) {
+           System.out.println("lowercase"); 
+        }
+       else{
+      System.out.println("uppercase");
+       }  
+       in.close();
+     }   
+    }
