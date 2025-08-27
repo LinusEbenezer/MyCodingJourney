d07703a171e8fbb247bb3bf7a720440eb9c99f6d
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-27 14:13:06 -0700

    Comprehensive 'Positive, Negative, Or Zero' Assessment In Java

---
+++ Java/Day_3/Task_8.java
@@ -0,0 +1,8 @@
+package MyCodingJourney.Java.Day_3;
+//Comprehensive 'Positive, Negative, Or Zero' Assessment In Java
+public class Task_8 {
+    public static void checkNumber(int number){
+System.out.println((number>0)?"positive":(number<0)?"negative":"zero");
+return;
+}    
+}
