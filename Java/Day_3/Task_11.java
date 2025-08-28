759e1ae277f763028f5a2082c10c7b2c8f84305b
Linus Ebenezer <linuslinus2907@gmail.com>
2025-08-27 20:07:38 -0700

    Leap year

---
+++ Java/Day_3/Task_11.java
@@ -0,0 +1,30 @@
+package MyCodingJourney.Java.Day_3;
+
+public class Task_11 {
+    public static void main(String[] args) {
+        isLeapYear(2024);
+    }
+public static boolean isLeapYear(int year){
+        if(year <1 || year>9999){
+            return false;
+        }
+            if(year%4==0) {
+                if (year%100!=0) {
+                        return true;
+                }      
+                else {
+                     if (year%400==0){    
+             return true; 
+               } else{
+                    return false;
+                }
+            }
+            }
+    else {
+        return false;
+
+    }
+    
+}
+}
+    
