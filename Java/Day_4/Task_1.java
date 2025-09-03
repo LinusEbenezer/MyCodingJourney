4f479db00b04506f93b52dd2898cb2d8bb9848fe
Linus Ebenezer <linuslinus2907@gmail.com>
2025-09-03 02:25:52 -0700

    Traditional Switch

---
+++ Java/Day_4/Task_1.java
@@ -0,0 +1,30 @@
+package MyCodingJourney.Java.Day_4;
+
+public class Task_1 {
+    public static void main(String[] args) {
+    
+        char nato = 'A';
+        switch (nato) {
+            case 'A':
+                System.out.println("Able");
+                break;
+            case 'B':
+            System.out.println("Brake");
+            break;
+            case 'C':
+            System.out.println("Charlie");
+            break;
+            case 'D':
+            System.out.println("Dog");
+            break;
+            case 'E':
+            System.out.println("Easy");
+            break;
+            default:
+            System.out.println("not found");
+                break;
+        }
+
+
+    }
+}
