1c5dd37787c11de7439919359785f5f0b9e85a1f
Linus Ebenezer <linuslinus2907@gmail.com>
2025-09-10 00:57:32 -0700

    Print the Array of Sting Using Join()

---
+++ Java/Day_4/StringJoin.java
@@ -0,0 +1,9 @@
+package MyCodingJourney.Java.Day_4;
+public class StringJoin {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Mango"};
+
+        System.out.println(String.join(", ", fruits));
+ 
+    }
+}
