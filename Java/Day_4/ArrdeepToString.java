5a1f32be122618073d4ba223be1ffd46d6143fc0
Linus Ebenezer <linuslinus2907@gmail.com>
2025-09-10 01:01:16 -0700

    Print the Array of multi dimension using Array deep To String

---
+++ Java/Day_4/ArrdeepToString.java
@@ -0,0 +1,10 @@
+package MyCodingJourney.Java.Day_4;
+
+import java.util.Arrays;
+
+public class ArrdeepToString {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2}, {3, 4}};
+        System.out.println(Arrays.deepToString(matrix));
+    }
+}
