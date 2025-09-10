d037d032da185b8023383159590870fdba5033c0
Linus Ebenezer <linuslinus2907@gmail.com>
2025-09-10 00:26:12 -0700

    Print arrays using For Loop

---
+++ Java/Day_4/ArrayInForLoop.java
@@ -0,0 +1,12 @@
+package MyCodingJourney.Java.Day_4;
+
+public class ArrayInForLoop {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+for (int i = 0; i < numbers.length; i++) {
+    System.out.println("Index " + i + ": " + numbers[i]);
+}
+
+    }
+}
