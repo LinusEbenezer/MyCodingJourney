e272d96058f0a946841ef5efef15666680023088
Linus Ebenezer <linuslinus2907@gmail.com>
2025-09-10 00:36:33 -0700

    Print Array Using To String

---
+++ Java/Day_4/ArrToString.java
@@ -0,0 +1,13 @@
+package MyCodingJourney.Java.Day_4;
+ import java.util.Arrays;
+public class ArrToString {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println(Arrays.toString(numbers));
+        
+        String[] fruits = {"Apple", "Banana", "Mango"};
+        System.out.println(Arrays.toString(fruits));
+
+    }
+}
